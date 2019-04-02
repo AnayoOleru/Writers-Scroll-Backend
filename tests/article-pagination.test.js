12f7e1ca5861d5a3760e99cae3f6a48913588b72
Akinwale Samuel <wale0331@gmail.com>
2019-04-03 00:14:13 +0100

    fix(test): add page number to article endpoint

--- tests/article-pagination.test.js
+++ tests/article-pagination.test.js
@@ -5,7 +5,7 @@ describe('Display articles in pages', () => {
   it('should display articles based on the page number', done => {
     chai
       .request(app)
-      .get('/api/v1/articles/:page')
+      .get('/api/v1/articles/1')
       .end((err, res) => {
         const { articles } = JSON.parse(res.text);
         expect(articles).to.be.a('array');

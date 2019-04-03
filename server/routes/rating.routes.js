b9ec335e02d85e0143cca241f6ac776a764f1b6d
mchardex <mchardex1995@gmail.com>
2019-04-03 12:46:46 +0100

    bg(fix):fix nested try catch
    another fix

--- server/routes/rating.routes.js
+++ server/routes/rating.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import controllers from '../controllers';
+import ratingValidation from '../schema/ratingSchema';
 
 const { ratingController } = controllers;
 
 const router = express.Router();
 
-router.post('/rating', ratingController.post);
+router.post('/rating', ratingValidation, ratingController.post);
 
 export default router;

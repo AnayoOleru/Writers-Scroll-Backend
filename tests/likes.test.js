2d8a5a5e5d1e2c6cc415463c5a7a3f495ef9d181
Amaechi Chuks <amaechichuks2000@yahoo.com>
2019-04-04 12:11:28 +0100

    feat(like-dislike): Resolve merge conflict

--- tests/likes.test.js
+++ tests/likes.test.js
@@ -21,29 +21,4 @@ describe('LIKE', () => {
         done();
       });
   });
-  it('should respone with 400 status for invalid user Id  ', done => {
-    chai
-      .request(app)
-      .post('/api/v1/likes/7139d3af-b8b4-44f6-a49f-9305791700f4')
-      .set('Authorization', token)
-      .send({
-        userId: '6517a6ea-662b-4eef-ab9f-20f89bd709', // Id from the seeder
-      })
-      .end((err, res) => {
-        expect(res.body).to.be.a('object');
-        expect(res.body.message).to.equal('id not valid');
-        done();
-      .request(app)
-      .post('/api/v1/likes/7139d3af-b8b4-44f6-a49f-9305791700f')
-      .set('Authorization', token)
-      .send({
-        userId: '6517a6ea-662b-4eef-ab9f-20f89bd7099c', // Id from the seeder
-      })
-      .end((err, res) => {
-        expect(res.body).to.be.a('object');
-        expect(res.body.status).to.be.equal(400);
-        expect(res.body.message).to.equal('id not valid');
-        done();
-      });
-  });
 });

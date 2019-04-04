de7fb134aa35801c78340e3c9aee01cf269603cd
AKaniru Victory <vic3coorp@gmail.com>
2019-04-04 12:12:57 +0100

    bg(controllers): fix no import issue on reset pass

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,12 @@ import profileController from './profile.controllers';
 import articleController from './article.controllers';
 import authController from './auth.controller';
 import ratingController from './rating.controller';
+import ResetPasswordController from './resetpassword';
 
 export default {
   profileController,
   articleController,
   authController,
   ratingController,
+  ResetPasswordController,
 };

7292faed25d5c99ad4808e3193f56ca886daceea
Anayo Oleru <anayooleru@Anayos-MacBook-Pro.local>
2019-04-04 18:06:53 +0100

    feature(tagsHelper):user tag articles[Finishes #164798025]

---
+++ server/helpers/tagsHelpers.js
@@ -0,0 +1,26 @@
+import models from '../models';
+
+const { Keyword } = models;
+const serverError = 'Server error, please try again later';
+
+const tagging = {
+  /**
+   *
+   * @param {*} articleId
+   * @param {*} tags
+   * @returns {*}  userId, articleId and keywords in an array.
+   */
+  async saveArticleTags(articleId, tags) {
+    try {
+      const Articletags = await Keyword.create({
+        article_id: articleId,
+        keyword: tags,
+      });
+      return Articletags;
+    } catch (err) {
+      return serverError;
+    }
+  },
+};
+
+export default tagging;

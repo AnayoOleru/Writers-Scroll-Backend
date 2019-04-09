0802ee2c29410185b40a908443c136fc686978e3
mchardex <mchardex1995@gmail.com>
2019-04-09 17:33:10 +0100

    write test

---
+++ server/documentation/swagger.js
@@ -0,0 +1,29 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+// swagger definition
+const swaggerDefinition = {
+  info: {
+    title: 'Hermes-AH Documentation',
+    version: '1.0.0',
+    description:
+      'A community of like-minded authors to foster inspiration and innovation by leveraging the modern web',
+  },
+  host: 'localhost:3000',
+  basePath: '/api/v1',
+  schemes: ['https', 'http'],
+  securityDefinitions: {
+    jwt: {
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header',
+    },
+  },
+  security: [{ jwt: [] }],
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc({
+  swaggerDefinition,
+  apis: ['./**/*.routes.js', './**/*/models/*.js'], // pass all in array
+});
+export default swaggerSpec;
